3219aab1c797b3d23a9134933ab5d9b7a9dd794f
Anton Maurovic <anton@foogle.com>
2024-11-06 15:39:01 +1030

    Progress, but Yosys not working with it yet

--- src/ring_osc.v
+++ src/ring_osc.v
@@ -12,11 +12,23 @@
 //      - https://skywater-pdk.readthedocs.io/en/main/contents/libraries/sky130_fd_sc_hd/cells/inv/README.html
 // - I was informed by my own former project:
 //      https://repositories.efabless.com/amm_efabless/ci2409_counter_and_vga3/blob/main/f/verilog/rtl/antenna_breaker.v
-//NOTE: Also need to make sure OpenLane RSZ_DONT_TOUCH_RX covers this.
-(* blackbox *) module sky130_fd_sc_hd__inv_2(
-    input A,
-    output Y // Inverted output.
+//NOTE: Also need to make sure OpenLane RSZ_DONT_TOUCH_RX covers this?
+// (* blackbox *) module sky130_fd_sc_hd__inv_2(
+//     input A,
+//     output Y // Inverted output.
+// );
+// endmodule
+
+module amm_inverter (
+    input   wire a,
+    output  wire y
 );
+
+    (* keep *) sky130_fd_sc_hd__inv_2   sky_inverter (
+        .A  (a),
+        .Y  (y)
+    );
+
 endmodule
 
 module ring_osc #(
@@ -28,9 +40,17 @@ module ring_osc #(
     wire [DEPTH*2:0] inv_in;
     wire [DEPTH*2:0] inv_out;
     assign inv_in[0] = inv_out[DEPTH*2]; // Loop back.
-    (* keep *) sky130_fd_sc_hd__inv_2 inv [DEPTH*2:0] (
-        .A(inv_in),
-        .Y(inv_out)
-    );
+    (* keep *) amm_inverter inv0  ( .a(inv_in[0 ]),  .y(inv_out[0 ]) );
+    (* keep *) amm_inverter inv1  ( .a(inv_in[1 ]),  .y(inv_out[1 ]) );
+    (* keep *) amm_inverter inv2  ( .a(inv_in[2 ]),  .y(inv_out[2 ]) );
+    (* keep *) amm_inverter inv3  ( .a(inv_in[3 ]),  .y(inv_out[3 ]) );
+    (* keep *) amm_inverter inv4  ( .a(inv_in[4 ]),  .y(inv_out[4 ]) );
+    (* keep *) amm_inverter inv5  ( .a(inv_in[5 ]),  .y(inv_out[5 ]) );
+    (* keep *) amm_inverter inv6  ( .a(inv_in[6 ]),  .y(inv_out[6 ]) );
+    (* keep *) amm_inverter inv7  ( .a(inv_in[7 ]),  .y(inv_out[7 ]) );
+    (* keep *) amm_inverter inv8  ( .a(inv_in[8 ]),  .y(inv_out[8 ]) );
+    (* keep *) amm_inverter inv9  ( .a(inv_in[9 ]),  .y(inv_out[9 ]) );
+    (* keep *) amm_inverter inv10 ( .a(inv_in[10]),  .y(inv_out[10]) );
+    assign osc_out = inv_in[0];
 
 endmodule
